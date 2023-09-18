bf1a291cbde3d2d5427eff683485de5ff6b83802
kymang <140633527+kymang@users.noreply.github.com>
2023-09-19 01:47:20 +0700

    Update start.py

--- Magic/assistant/start.py
+++ Magic/assistant/start.py
@@ -27,26 +27,3 @@ async def start_(client: Client, message: Message):
         ),
      disable_web_page_preview=True
     )
-
-@bot.on_message(filters.command(["clone"]))
-async def add_ubot(client: Client, msg: Message):
-    chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone session")
-    
-    if len(msg.command) < 2:
-        await text.edit("Usage:\n\n /clone session\nPlease provide a session string.")
-        return
-    
-    cmd = msg.command
-    phone = msg.command[1]
-    
-    try:
-        await text.edit("Booting Your Client")
-        # Change this directory according to your repository
-        client = Client(name="ubot", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Magic/plugins"))
-        await client.start()
-        user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully As {user.first_name} ✅.")
-    except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
-
