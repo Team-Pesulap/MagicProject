5a0f559f41b0e6fb5dd4399b46a878bdf590e0fc
kymang <140633527+kymang@users.noreply.github.com>
2023-09-19 00:25:45 +0700

    Update vctools.py

--- Magic/plugins/vctools.py
+++ Magic/plugins/vctools.py
@@ -53,7 +53,7 @@ async def leavevc(client, message):
     with suppress(ValueError):
         chat_id = int(chat_id)
     try:
-        await client.vc.leave()
+        await client.vc.stop()
     except Exception as f:
         return await edit_or_reply(message, f"**ERROR:** `{f}`")
     msg = "**Successfully leave the Voice Chat**\n**"
