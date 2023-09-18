cf37e4bc1bfc3560d9564f2b0411dfbcbffe0409
kymang <140633527+kymang@users.noreply.github.com>
2023-09-19 01:06:29 +0700

    Update ping.py

--- Magic/plugins/ping.py
+++ Magic/plugins/ping.py
@@ -33,8 +33,7 @@ async def twing(client: Client, message: Message):
 @ubot.on_message(filters.command("ping", prefix) & filters.me)
 async def pinx(client: Client, message: Message):
     mulai = datetime.now()
-    berhenti = datetime.now()
-    durasi = (mulai - berhenti).microseconds / 1000
+    durasi = (mulai - mulai).microseconds / 100
     await message.reply_text(f"**Sepong!**\n" f"`%sms`" % (durasi))
 
 
